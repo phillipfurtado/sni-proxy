ee9444503b1ef5ae1e895c2b86e300b24870e781
Phillip Furtado <phillip.furtado@gmail.com>
2016-02-25 01:25:52 -0400

    Garantido que existe a pasta ssl no /etc/nginx

--- proxy.sh
+++ proxy.sh
@@ -7,7 +7,10 @@ sudo apt-get install -y --force-yes nginx
 echo Configurando NGINX com proxy reverso SNI
 
 sudo cp sites-available/* /etc/nginx/conf.d/
-sudo cp ssl/* /etc/nginx/ssl/
+
+sudo mkdir -p /etc/nginx/ssl
+
+sudo cp ssl/** /etc/nginx/ssl/
 
 echo Iniciando NGINX
 sudo service nginx restart
